a7d683596a1b6960a14fd1a4c0e298776d7a214f
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2021-10-04 13:19:53 +0600

    blog project

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+
+from .models import Post
+
+
+class HomePageView(ListView):
+    model = Post
+    context_object_name = "all_post"
+    template_name = "home.html"
