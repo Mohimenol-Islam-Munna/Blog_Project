25aaa3859499dca82cfaadb50fc26912c7bffe2d
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2021-11-02 10:17:59 +0600

    add static files

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.db.models.fields import CharField, TextField
 from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
-
-
 class Post(models.Model):
     title = CharField(max_length=200)
     author = ForeignKey("auth.User", on_delete=models.CASCADE)
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from django.views.generic import TemplateView, ListView
 
 from .models import Post
 
-
 class HomePageView(ListView):
     model = Post
     context_object_name = "all_post"
